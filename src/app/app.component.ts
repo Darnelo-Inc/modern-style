4f3eaf680748ad02aae0f1df5aef42b0ce0f054e
Pavel Gorbunov <darnelo@inbox.ru>
2023-08-20 17:03:07 +0500

    product component with directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { IProduct } from "./models/product"
+import { products as data } from "./data/products"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'modern-style';
+  title = "modern-style"
+
+  products: IProduct[] = data
 }
