49b7547403977c8d133bc300bbb73a1af4cefde8
Pavel Gorbunov <darnelo@inbox.ru>
2023-08-23 16:06:42 +0500

    modal & create-product-modal components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { ProductComponent } from "./components/product/product.component"
 import { HttpClientModule } from "@angular/common/http"
 import { GlobalErrorComponent } from "./components/global-error/global-error.component"
 import { FormsModule } from "@angular/forms";
-import { SearchProductsPipe } from './pipes/search-products.pipe'
+import { SearchProductsPipe } from './pipes/search-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductModalComponent } from './components/create-product-modal/create-product-modal.component'
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, GlobalErrorComponent, SearchProductsPipe],
+  declarations: [AppComponent, ProductComponent, GlobalErrorComponent, SearchProductsPipe, ModalComponent, CreateProductModalComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/create-product-modal/create-product-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-product-modal',
+  templateUrl: './create-product-modal.component.html',
+  styleUrls: ['./create-product-modal.component.scss']
+})
+export class CreateProductModalComponent {
+
+}
