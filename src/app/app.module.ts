87dd1f05835b757c9f1473253ed9a56eeb16671f
Pavel Gorbunov <darnelo@inbox.ru>
2023-08-23 19:25:01 +0500

    modal form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,27 @@ import { AppComponent } from "./app.component"
 import { ProductComponent } from "./components/product/product.component"
 import { HttpClientModule } from "@angular/common/http"
 import { GlobalErrorComponent } from "./components/global-error/global-error.component"
-import { FormsModule } from "@angular/forms";
-import { SearchProductsPipe } from './pipes/search-products.pipe';
-import { ModalComponent } from './components/modal/modal.component';
-import { CreateProductModalComponent } from './components/create-product-modal/create-product-modal.component'
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { SearchProductsPipe } from "./pipes/search-products.pipe"
+import { ModalComponent } from "./components/modal/modal.component"
+import { CreateProductModalComponent } from "./components/create-product-modal/create-product-modal.component"
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, GlobalErrorComponent, SearchProductsPipe, ModalComponent, CreateProductModalComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    GlobalErrorComponent,
+    SearchProductsPipe,
+    ModalComponent,
+    CreateProductModalComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/create-product-modal/create-product-modal.component.ts
+++ src/app/components/create-product-modal/create-product-modal.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { FormControl, FormGroup, Validators } from "@angular/forms"
 
 @Component({
-  selector: 'app-create-product-modal',
-  templateUrl: './create-product-modal.component.html',
-  styleUrls: ['./create-product-modal.component.scss']
+  selector: "app-create-product-modal",
+  templateUrl: "./create-product-modal.component.html",
+  styleUrls: ["./create-product-modal.component.scss"],
 })
 export class CreateProductModalComponent {
+  form = new FormGroup({
+    title: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  })
 
+  submitHandler() {
+    console.log(this.form.value)
+  }
+
+  get title() {
+    return this.form.controls.title
+  }
 }
